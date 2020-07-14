96bd22ad9386349771a04501c35508cb18e6335d
Joao Lucas Silva <joaolucassalvatore@gmail.com>
2020-07-14 19:32:37 -0300

    Projeto criado

---
+++ src/test/java/com/joaolucas/cursomc/CursomcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joaolucas.cursomc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
