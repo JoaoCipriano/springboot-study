c66c9236c30c8634949f223568db9e03768884b9
Joao Lucas Silva <joaolucassalvatore@gmail.com>
2021-02-05 15:44:07 -0300

    Endpoint /pedidos/{id} disponibilizado

--- src/main/java/com/joaolucas/cursomc/services/PedidoService.java
+++ src/main/java/com/joaolucas/cursomc/services/PedidoService.java
@@ -3,20 +3,19 @@ package com.joaolucas.cursomc.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.joaolucas.cursomc.domain.Categoria;
-import com.joaolucas.cursomc.repositories.CategoriaRepository;
+import com.joaolucas.cursomc.domain.Pedido;
+import com.joaolucas.cursomc.repositories.PedidoRepository;
 import com.joaolucas.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
-public class CategoriaService {
+public class PedidoService {
 	
 	@Autowired
-	private CategoriaRepository repo;
+	private PedidoRepository repo;
 	
-	public Categoria find(Integer id) {
+	public Pedido find(Integer id) {
 		return repo.findById(id)
 				.orElseThrow(() -> new ObjectNotFoundException(
-						"Objeto não encontrado Id: " + id + ", Tipo: " + Categoria.class.getName()));
+						"Objeto não encontrado Id: " + id + ", Tipo: " + Pedido.class.getName()));
 	}
-	
 }
