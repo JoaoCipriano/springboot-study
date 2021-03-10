d5d1fe2c0e16cfbc2da2b2864f9ded1b8babfa59
Joao Lucas Silva <joaolucassalvatore@gmail.com>
2021-03-10 12:38:01 -0300

    Paginação com parametros opcionais na requisição

--- src/main/java/com/joaolucas/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/joaolucas/cursomc/resources/CategoriaResource.java
@@ -2,6 +2,7 @@ package com.joaolucas.cursomc.resources;
 
 import com.joaolucas.cursomc.dto.CategoriaDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -28,11 +29,22 @@ public class CategoriaResource {
 
 	@GetMapping()
 	public ResponseEntity<List<CategoriaDTO>> findAll() {
-		List<CategoriaDTO> list = service.findAll()
+		List<CategoriaDTO> listDto = service.findAll()
 				.stream()
 				.map(CategoriaDTO::new)
 				.collect(Collectors.toList());
-		return ResponseEntity.ok().body(list);
+		return ResponseEntity.ok().body(listDto);
+	}
+
+	@GetMapping(value="/page")
+	public ResponseEntity<Page<CategoriaDTO>> findPage(
+			@RequestParam(value="page", defaultValue="0") Integer page,
+			@RequestParam(value="linesPerPage", defaultValue="24") Integer linesPerPage,
+			@RequestParam(value="orderBy", defaultValue="nome") String orderBy,
+			@RequestParam(value="direction", defaultValue="ASC") String direction) {
+		Page<CategoriaDTO> listDto = service.findPage(page, linesPerPage, orderBy, direction)
+				.map(CategoriaDTO::new);
+		return ResponseEntity.ok().body(listDto);
 	}
 
 	@PostMapping
--- src/main/java/com/joaolucas/cursomc/services/CategoriaService.java
+++ src/main/java/com/joaolucas/cursomc/services/CategoriaService.java
@@ -3,6 +3,9 @@ package com.joaolucas.cursomc.services;
 import com.joaolucas.cursomc.services.exceptions.DataIntegrityException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import com.joaolucas.cursomc.domain.Categoria;
@@ -40,6 +43,11 @@ public class CategoriaService {
 		}catch (DataIntegrityViolationException e) {
 			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos");
 		}
+	}
+
+	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Sort.Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
 	}
 
 	public List<Categoria> findAll() {
