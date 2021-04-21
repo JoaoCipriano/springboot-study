81eb07b9888f9a11994d3a5086b158cb40719b7a
Joao Lucas Silva <joaolucassalvatore@gmail.com>
2021-04-21 20:01:54 -0300

    Fix the time zone and make the date format available to the entire application

--- src/main/java/com/joaolucas/cursomc/config/JacksonConfig.java
+++ src/main/java/com/joaolucas/cursomc/config/JacksonConfig.java
@@ -7,9 +7,15 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Locale;
+
 @Configuration
 public class JacksonConfig {
     // https://stackoverflow.com/questions/41452598/overcome-can-not-construct-instance-ofinterfaceclass-without-hinting-the-pare
+
+    private final DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm", new Locale("pt", "BR"));
 
     @Bean
     public Jackson2ObjectMapperBuilder objectMapperBuilder() {
@@ -18,6 +24,7 @@ public class JacksonConfig {
             public void configure(ObjectMapper objectMapper) {
                 objectMapper.registerSubtypes(PagamentoComCartao.class);
                 objectMapper.registerSubtypes(PagamentoComBoleto.class);
+                objectMapper.setDateFormat(dateFormat);
                 super.configure(objectMapper);
             }
         };
