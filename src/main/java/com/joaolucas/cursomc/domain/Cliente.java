6dac5e8670be28db5ee7614d92a6590a8320662e
Joao Lucas Silva <joaolucassalvatore@gmail.com>
2021-06-16 20:21:08 -0300

    Usando padrão de nomes para imagens

--- src/main/java/com/joaolucas/cursomc/domain/Cliente.java
+++ src/main/java/com/joaolucas/cursomc/domain/Cliente.java
@@ -46,8 +46,6 @@ public class Cliente implements Serializable{
 	@CollectionTable(name = "PERFIS")
 	private Set<Integer> perfis = new HashSet<>();
 
-	private String imageUrl;
-	
 	public Cliente() {
 		addPerfil(Perfil.CLIENTE);
 	}
@@ -172,13 +170,5 @@ public class Cliente implements Serializable{
 		} else if (!id.equals(other.id))
 			return false;
 		return true;
-	}
-
-	public String getImageUrl() {
-		return imageUrl;
-	}
-
-	public void setImageUrl(String imageUrl) {
-		this.imageUrl = imageUrl;
 	}
 }
--- src/main/java/com/joaolucas/cursomc/services/ClienteService.java
+++ src/main/java/com/joaolucas/cursomc/services/ClienteService.java
@@ -13,6 +13,7 @@ import com.joaolucas.cursomc.services.exceptions.AuthorizationException;
 import com.joaolucas.cursomc.services.exceptions.DataIntegrityException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -25,6 +26,7 @@ import com.joaolucas.cursomc.repositories.ClienteRepository;
 import com.joaolucas.cursomc.services.exceptions.ObjectNotFoundException;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.awt.image.BufferedImage;
 import java.net.URI;
 import java.util.List;
 
@@ -42,6 +44,12 @@ public class ClienteService {
 
 	@Autowired
 	private S3Service s3Service;
+
+	@Autowired
+	private ImageService imageService;
+
+	@Value("${img.prefix.client.profile}")
+	private String prefix;
 	
 	public Cliente find(Integer id) {
 		UserSS user = UserService.authenticated();
@@ -123,12 +131,9 @@ public class ClienteService {
 		if (user == null)
 			throw new AuthorizationException("Acesso negado");
 
-		URI uri = s3Service.uploadFile(multipartFile);
+		BufferedImage jpgImage = imageService.getJpgImageFromFile(multipartFile);
+		String fileName = prefix + user.getId() + ".jpg";
 
-		Cliente cli = repo.findById(user.getId()).orElseThrow(null);
-		cli.setImageUrl(uri.toString());
-		repo.save(cli);
-
-		return uri;
+		return s3Service.uploadFile(imageService.getInputStream(jpgImage, "jpg"), fileName, "image");
 	}
 }
