c068c93fc10ac9d92c2c7f5c5dcc755d14f826f4
Joao Cipriano <joaolucassalvatore@gmail.com>
2023-12-19 21:38:54 -0300

    Remove references to DBService class

--- src/main/java/com/joaolucas/study/core/configuration/TestConfiguration.java
+++ src/main/java/com/joaolucas/study/core/configuration/TestConfiguration.java
@@ -1,6 +1,5 @@
 package com.joaolucas.study.core.configuration;
 
-import com.joaolucas.study.domain.databasecharge.DBService;
 import com.joaolucas.study.infrastructure.email.EmailService;
 import com.joaolucas.study.infrastructure.email.impl.MockEmailService;
 import org.slf4j.Logger;
@@ -10,17 +9,9 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.thymeleaf.TemplateEngine;
 
-import java.text.ParseException;
-
 @Configuration
 @Profile("test")
 public class TestConfiguration {
-
-    @Bean
-    public boolean instantiateDatabase(DBService dbService) throws ParseException {
-        dbService.instantiateTestDatabase();
-        return true;
-    }
 
     @Bean
     public EmailService emailService(TemplateEngine templateEngine, JavaMailSender javaMailSender, Logger logger) {
